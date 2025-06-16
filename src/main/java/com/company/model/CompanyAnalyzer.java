0fd43d60327a83c5301e52c66e2fe310d1bc5d1c
Prachi Jindal <prachibansal135@gmail.com>
2025-06-16 17:24:39 +0200

    CompanyAnalyzer.Java class implemented

---
+++ src/main/java/com/company/model/CompanyAnalyzer.java
@@ -0,0 +1,46 @@
+package com.company.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class CompanyAnalyzer {
+
+    /**
+     * Flattens the list of projects from a list of employees.
+     * List<Employee> employees; // Each employee has a List<Project> projects.
+     */
+    public List<Project> getAllProjeccts(List<Employee> employees) {
+        return employees.stream()
+                .flatMap(employee -> employee.getProjects().stream())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Groups projects by status and counts them.
+     * GroupingBy creates a Map<String, Long> where keys are
+     * project statuses and values are counts
+     */
+    public Map<String, Long> countProjectsByStatus(List<Project> projects) {
+        return projects.stream()
+                .collect(Collectors.groupingBy(Project::getStatus, Collectors.counting()));
+    }
+
+
+    /**
+     * Calculate average budget by project status.
+     * Groups Projects by status and calculate the average budget per status.
+     */
+    public Map<String,Double> averageBudgetByStatus(List<Project> projects){
+        return projects.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Project::getStatus, Collectors.averagingDouble(
+                                        Project::getBudget)
+                        )
+                );
+    }
+
+
+}
